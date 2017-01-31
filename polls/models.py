508baf33c644c8fee075ce7c866594e17e20b2e8
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-31 15:51:52 +0700

    ganti model

--- polls/models.py
+++ polls/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Dhimas(models.Model):
+    dhimas_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('data published')
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+class Ganteng(models.Model):
+    dhimas = models.ForeignKey(Dhimas, on_delete=models.CASCADE)
+    ganteng_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
